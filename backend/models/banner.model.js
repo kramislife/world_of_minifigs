daa9801b623060fa0c95fed1672ee15e11a1d65b
kramislyf <egariando619@gmail.com>
2025-03-11 12:15:32 +0800

    Add banner management functionality to frontend and backend

---
+++ backend/models/banner.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const bannerSchema = new mongoose.Schema(
+  {
+    image: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    updated_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Banner = mongoose.model("Banner", bannerSchema);
+export default Banner;
