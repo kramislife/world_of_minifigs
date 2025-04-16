80f073565a21616cf588ecc9a77c81a39e30a4a8
kramislyf <egariando619@gmail.com>
2025-04-16 09:32:16 +0800

    refactor cart sheet components

--- frontend/src/components/layout/header/CartSheet.jsx
+++ frontend/src/components/layout/header/CartSheet.jsx
@@ -2,12 +2,12 @@ import {
   Sheet,
   SheetContent,
   SheetDescription,
+  SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet";
 import LoadingSpinner from "@/components/layout/spinner/LoadingSpinner";
 import { ShoppingCart, ArrowRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import CartHeader from "./components/CartHeader";
 import CartItem from "./components/CartItem";
 import CartFooter from "./components/CartFooter";
 import { useCartSheet } from "@/hooks/Product/useCartSheet";
@@ -30,19 +30,18 @@ const CartSheet = ({ isOpen, setIsOpen }) => {
   const CartEmpty = () => {
     return (
       <div className="flex flex-col items-center justify-center text-center p-5">
-        <div className="bg-brand-dark/50 rounded-full p-8 mb-6">
+        <div className="bg-brand-dark/50 rounded-full p-8">
           <ShoppingCart size={48} className="text-gray-400" />
         </div>
-        <h3 className="text-xl font-semibold text-white mb-2">
+        <h3 className="text-xl font-semibold text-white mt-5 mb-2">
           Your cart is empty
         </h3>
-        <p className="text-gray-400 mb-6">
+        <p className="text-gray-400 mb-6 text-sm md:text-base leading-6">
           Looks like you haven't added anything to your cart yet. Explore our
           top products and build up with awesome bricks!
         </p>
         <Button
-          variant="buyNow"
-          className="w-[200px]"
+          variant="accent"
           onClick={() => {
             navigate("/products");
             setIsOpen(false);
@@ -57,51 +56,56 @@ const CartSheet = ({ isOpen, setIsOpen }) => {
 
   return (
     <Sheet open={isOpen} onOpenChange={setIsOpen}>
-      <SheetContent className="w-full max-w-[400px] lg:max-w-[600px] bg-brand-start p-0">
-        <SheetTitle className="sr-only">Shopping Cart</SheetTitle>
-        <SheetDescription className="sr-only">
-          View your shopping cart items, adjust quantities, and proceed to
-          checkout.
-        </SheetDescription>
-
-        <div className="h-full flex flex-col">
-          <CartHeader />
-
-          <div className="flex-1 overflow-y-auto">
-            <div className="md:p-5 px-3 py-5 space-y-5">
-              {isLoading ? (
-                <div className="flex justify-center items-center py-20">
-                  <LoadingSpinner height="h-full" />
-                </div>
-              ) : updatedCartItems.length === 0 ? (
-                <CartEmpty />
-              ) : (
-                <div>
-                  <ul className="space-y-5">
-                    {updatedCartItems.map((item) => (
-                      <CartItem
-                        key={item.product}
-                        item={item}
-                        onQuantityUpdate={handleQuantityUpdate}
-                      />
-                    ))}
-                  </ul>
-                </div>
-              )}
+      <SheetContent>
+        <SheetHeader>
+          <div className="flex items-center gap-3">
+            <div className="p-2 bg-accent rounded-lg">
+              <ShoppingCart size={20} className="text-foreground" />
             </div>
+            <SheetTitle className="m-0 sticky top-0 z-50">
+              Shopping Cart
+            </SheetTitle>
           </div>
+          <SheetDescription className="sr-only">
+            View your shopping cart items, adjust quantities, and proceed to
+            checkout.
+          </SheetDescription>
+        </SheetHeader>
 
-          {!isLoading && updatedCartItems.length > 0 && (
-            <CartFooter
-              total={total}
-              onCheckout={handleCheckout}
-              showLoginMessage={showLoginMessage}
-              checkoutDisabled={checkoutDisabled}
-              isAuthenticated={isAuthenticated}
-              hasOutOfStockItems={hasOutOfStockItems}
-            />
-          )}
+        <div className="flex-1 overflow-y-auto">
+          <div className="p-5">
+            {isLoading ? (
+              <div className="flex justify-center items-center py-20">
+                <LoadingSpinner height="h-full" />
+              </div>
+            ) : updatedCartItems.length === 0 ? (
+              <CartEmpty />
+            ) : (
+              <div>
+                <ul className="space-y-5">
+                  {updatedCartItems.map((item) => (
+                    <CartItem
+                      key={item.product}
+                      item={item}
+                      onQuantityUpdate={handleQuantityUpdate}
+                    />
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
+
+        {!isLoading && updatedCartItems.length > 0 && (
+          <CartFooter
+            total={total}
+            onCheckout={handleCheckout}
+            showLoginMessage={showLoginMessage}
+            checkoutDisabled={checkoutDisabled}
+            isAuthenticated={isAuthenticated}
+            hasOutOfStockItems={hasOutOfStockItems}
+          />
+        )}
       </SheetContent>
     </Sheet>
   );
