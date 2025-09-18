3d2ea5a4a772780306eb65f547bcbd563bab8850
Animesh Jha <markstorey.wrc@gmail.com>
2025-09-18 23:37:09 +0200

    added code for minifig-builder and other optimization in vite-config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,40 @@
-import path from "path";
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { build, defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    proxy: { "/api": "http://localhost:4000/" },
-    changeOrigin: true,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	server: {
+		proxy: {
+			'/api': 'http://localhost:4000/',
+		},
+		changeOrigin: true,
+	},
+
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('react') || id.includes('react-dom')) {
+							return 'react-vendor';
+						}
+						if (id.includes('axios') || id.includes('loadash')) {
+							return 'vendor';
+						}
+						return 'lib';
+					}
+				},
+				entryFileNames: 'assets/[name]-[hash].js',
+				chunkFileNames: 'assets/[name]-[hash].js',
+				assetFileNames: 'assets/[name]-[hash].[ext]',
+			},
+		},
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
